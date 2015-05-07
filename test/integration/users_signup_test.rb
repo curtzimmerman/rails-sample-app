40bb8ec11193c941000e2f0f1b24050ab29a3325
Curt Zimmerman <czimmerman9@gmail.com>
2015-05-06 20:40:40 -0400

    add user signup

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  
+	test "invalid signup information" do
+	  assert_no_difference 'User.count' do
+	  	get signup_path
+	  	post users_path, user: { name: "", email: "user@invalid", password: "foo", password_confirmation: "bar" }
+	  end
+	  assert_template 'users/new'
+	end
+
+	test "valid signup information" do
+		assert_difference 'User.count', 1 do
+			get signup_path
+			post_via_redirect users_path, user: { name: "Testing", email: "test@email.com", password: "123456", password_confirmation: "123456" }
+		end
+		assert_template 'users/show'
+	end
+end
