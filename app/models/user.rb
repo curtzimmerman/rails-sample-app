7148144c95c25d85b993bc6125901c53d21922aa
Curt Zimmerman <czimmerman9@gmail.com>
2015-05-14 20:24:16 -0400

    create user authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+	attr_accessor :remember_token
+
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
 	before_save { email.downcase! }
@@ -19,5 +21,26 @@ class User < ActiveRecord::Base
 		BCrypt::Password.create(string, cost: cost)
 	end
 
+	#returns a random token
+	def User.new_token
+		SecureRandom.urlsafe_base64
+	end
+
+	#remembers a user in the database for use in persistent sessions
+	def remember
+		self.remember_token = User.new_token
+		update_attribute(:remember_digest, User.digest(remember_token))
+	end
+
+	#returns true if the given token matches the digest
+	def authenticated?(remember_token)
+		return false if remember_digest.nil?
+		BCrypt::Password.new(remember_digest).is_password?(remember_token)
+	end
+
+	#forgets a user
+	def forget
+		update_attribute(:remember_digest, nil)
+	end
 	
 end
